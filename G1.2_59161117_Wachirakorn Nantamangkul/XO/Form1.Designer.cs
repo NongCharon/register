920c95fa28191bf7e9b21d4a513478fa52779965
bankbetch <jiraphun.jrb@gmail.com>
2018-09-15 11:01:20 +0700

    Finish

---
+++ G1.2_59161117_Wachirakorn Nantamangkul/XO/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.btnRegister = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRegister)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(270, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.txtUsername.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUsername.Location = new System.Drawing.Point(70, 168);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(195, 28);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TabStop = false;
+            this.txtUsername.Text = "Username";
+            this.txtUsername.Click += new System.EventHandler(this.txtUsername_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(426, 335);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPassword.Location = new System.Drawing.Point(70, 225);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(195, 28);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TabStop = false;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtConfirmPassword.Location = new System.Drawing.Point(70, 283);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(195, 28);
+            this.txtConfirmPassword.TabIndex = 3;
+            this.txtConfirmPassword.TabStop = false;
+            this.txtConfirmPassword.Text = "Confirm Password";
+            this.txtConfirmPassword.Click += new System.EventHandler(this.txtComfirmPassword_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Image = global::XO.Properties.Resources.register_close;
+            this.btnClose.Location = new System.Drawing.Point(277, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(19, 27);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnClose.TabIndex = 4;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegister.Image = global::XO.Properties.Resources.register_register;
+            this.btnRegister.Location = new System.Drawing.Point(161, 336);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(110, 35);
+            this.btnRegister.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.TabStop = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Image = global::XO.Properties.Resources.register_reset;
+            this.btnReset.Location = new System.Drawing.Point(29, 336);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(110, 35);
+            this.btnReset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnReset.TabIndex = 6;
+            this.btnReset.TabStop = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(209, 403);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(300, 400);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRegister)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.PictureBox btnRegister;
+        private System.Windows.Forms.PictureBox btnReset;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ G1.2_59161117_Wachirakorn Nantamangkul/XO/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtConfirmPassword.Text = "";
+        }
+
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void txtComfirmPassword_Click(object sender, EventArgs e)
+        {
+            txtConfirmPassword.Text = "";
+            txtConfirmPassword.UseSystemPasswordChar = true;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MongoClient client = new MongoClient("mongodb://admin:a123456@ds141902.mlab.com:41902/ox");
+                MongoServer server = client.GetServer();
+                MongoDatabase database = server.GetDatabase("ox");
+                MongoCollection mongoCollection = database.GetCollection<User>("User");
+                User user = new User();
+                BindingList<User> doclist = new BindingList<User>();
+                var getCollection = database.GetCollection<User>("User");
+                var insertUser = getCollection.AsQueryable().Where(pd => pd.Username == txtUsername.Text);
+
+                foreach (var p in insertUser)
+                {
+                    doclist.Add(p);
+                    Application.DoEvents();
+                }
+                dataGridView1.DataSource = doclist;
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    if (!string.IsNullOrEmpty(txtUsername.Text.Trim()) && !txtUsername.Text.Equals("Username"))
+                    {
+                        user.Username = txtUsername.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Username");
+                    }
+                    if (!string.IsNullOrEmpty(txtPassword.Text.Trim()) && !txtPassword.Text.Equals("Password"))
+                    {
+                        user.Password = txtPassword.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Password");
+                    }
+                    if (!string.IsNullOrEmpty(txtConfirmPassword.Text.Trim()) && !txtConfirmPassword.Text.Equals("Confirm Password"))
+                    {
+                        user.Password = txtConfirmPassword.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอก Comfirm Password");
+                    }
+
+                    user.Name = txtUsername.Text;
+                    user.Avatar = null;
+                    user.Win = 0;
+                    user.Draw = 0;
+                    user.Lose = 0;
+
+                    if (!string.IsNullOrEmpty(txtUsername.Text.Trim()) &&
+                        !string.IsNullOrEmpty(txtPassword.Text.Trim()) &&
+                        !string.IsNullOrEmpty(txtConfirmPassword.Text.Trim()) &&
+                        (!txtUsername.Text.Equals("Username") &&
+                         !txtPassword.Text.Equals("Password") &&
+                         !txtConfirmPassword.Text.Equals("Confirm Password")))
+                    {
+                        if (txtPassword.Text.Equals(txtConfirmPassword.Text))
+                        {
+                            mongoCollection.Insert(user);
+                            MessageBox.Show("สมัครเสร็จสิ้น");
+                        }
+                        else
+                        {
+                            MessageBox.Show("กรุณาใส่ Password ให้ตรงกัน");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+
+    }
+}
